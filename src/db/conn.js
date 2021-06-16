2fb3146349acaa2b038662dfe06dd49d44226dc6
jay522 <jayrajverma7777@gmail.com>
2021-06-16 17:30:43 +0530

    redirect to home

--- src/db/conn.js
+++ src/db/conn.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const User = require("../models/post");
+const { response } = require("express");
 
 // creating db
 mongoose.connect("mongodb+srv://creator54:u4gmyE8zpQ2sdEq@cluster0.rzvyn.mongodb.net/blog-crud?retryWrites=true&w=majority", {
